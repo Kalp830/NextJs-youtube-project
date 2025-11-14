bf442289a9b48a5a61f741fee725232b0273d942
virupatel11 <viral.xelcomm@gmail.com>
2025-11-14 19:21:40 +0530

    Font learning

--- nextjs-learning/src/app/layout.js
+++ nextjs-learning/src/app/layout.js
@@ -1,10 +1,20 @@
 import Navigation from "./component/Navigation";
+import {Roboto, Work_Sans} from "next/font/google";
+import { Work_Sans } from "next/font/google";
 
+const roboto = Roboto({
+  subsets:["latin"],
+  variable:"--font-roboto",
+});
 
+const WorkSans = Work_Sans({
+   subsets:["latin"],
+   variable:"--font-work-sans",
+})
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${roboto.variable} ${WorkSans.variable}`}>
         <Navigation />
         {children}
       </body>
